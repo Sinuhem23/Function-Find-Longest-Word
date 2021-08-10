3046744c88af26d21169b996183db442a715853e
sinuhe Montero <sinuhem23@gmail.com>
2021-08-10 19:22:29 -0400

    updated the lonestword

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 ///////////////////////////////////////////////////////////////////
 var prompt = require('prompt-sync')();
 var user_Array = prompt('Enter something: ');
+let longestWord;
 
 function findLongestWord(str) {
   let words = str.split(' ');
@@ -9,9 +10,10 @@ function findLongestWord(str) {
   for (let i = 0; i < words.length; i++) {
     if (words[i].length > maxLength) {
       maxLength = words[i].length;
-      console.log('Longest word is: ' + words[i] + '.');
+      longestWord = words[i];
     }
   }
+  console.log('Longest word is: ' + longestWord + '.');
   return 'The length is: ' + maxLength;
 }
 console.log(findLongestWord(user_Array));
